012f5c9eb23369ba89f94eef994af82914188e76
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-01-15 21:03:32 +0100

    added a cpp environment to compile lib-usb

---
+++ cpp/manifests/init.pp
@@ -0,0 +1,31 @@
+package { 'git':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'libudev-dev':
+  ensure => installed
+}
+
+package { 'libusb-1.0-0-dev':
+  ensure => installed
+}
+
+package { 'libfox-1.6-dev':
+  ensure => installed
+}
+
+package { 'autotools-dev':
+  ensure => installed
+}
+
+package { 'autoconf':
+  ensure => installed
+}
+
+package { 'libtool':
+  ensure => installed
+}
