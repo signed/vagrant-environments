0a785a7813c363db97a68ef921216ca1c52fd1f2
Thomas Heilbronner <Thomas.Heilbronner@idos.de>
2014-01-16 10:46:12 +0100

    imporved manifest

--- jboss/puppet/manifests/site.pp
+++ jboss/puppet/manifests/site.pp
@@ -1,15 +1,40 @@
 #$packages = [ "git", "build-essential", "libxslt1-dev", libxml2-dev]
 #package { $packages: ensure => installed }
 
-package{ "puppet":
-  ensure=>latest,
-}
 
-class { 'java':
-  version => 'latest',
-}
+node default {
+  include apt
+  include stdlib
 
-package{ 'install unzip':
-  name => 'unzip',
-  ensure => 'present',
+  class{ 'java':
+    before => Class['jboss'],
+    version => 'latest',
+  }
+
+  package{ "install puppet":
+    before => Class['jboss'],
+    name =>  'puppet',
+    ensure=>latest,
+  }
+
+  package{ "install unzip":
+    before => Class['jboss'],
+    name => 'unzip',
+    ensure=>latest,
+  }
+
+
+  #change owner of extracted files
+  class { 'jboss':
+    install             => 'source',
+    version             => '6',
+    install_source      => 'http://192.168.9.138:8080/files/jboss/jboss-as-distribution-6.1.0.Final.zip',
+    disable  => '',
+  }
+
+  jboss::instance { 'strom':
+    createuser => false, # Default user jboss is already created by jboss class
+    bindaddr    => '0.0.0.0',
+    port        => '8082',
+  }
 }
